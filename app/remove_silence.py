cc0420d99b6046ec78c2d48df4c4e4fd9e4f6f6c
naoto kimura <108779431+naoto714714@users.noreply.github.com>
2024-11-16 02:29:12 +0000

    fix: パスではなく音声ファイルを返すように

--- app/remove_silence.py
+++ app/remove_silence.py
@@ -1,15 +1,13 @@
 # 参考：https://github.com/snakers4/silero-vad/wiki/Examples-and-Dependencies#examples
 from silero_vad import collect_chunks, get_speech_timestamps, load_silero_vad, read_audio, save_audio
 
-SAMPLING_RATE = 16000
+from main import SAMPLING_RATE
 
 
-def remove_silence(audio_file, output_path):
+def remove_silence(audio_file):
     model = load_silero_vad(onnx=False)
     audio = read_audio(audio_file, sampling_rate=SAMPLING_RATE)
 
     speech_timestamps = get_speech_timestamps(audio, model, sampling_rate=SAMPLING_RATE)
 
-    save_audio(output_path, collect_chunks(speech_timestamps, audio), SAMPLING_RATE)
-
-    return output_path
+    return collect_chunks(speech_timestamps, audio)
