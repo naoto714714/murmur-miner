7f267246c0b8d7c85c0dbffaea9a872699297151
naoto kimura <108779431+naoto714714@users.noreply.github.com>
2024-11-16 02:33:48 +0000

    fix: mainのリファクタ

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 from remove_silence import remove_silence
@@ -6,30 +5,30 @@ from speech_to_text import speech_to_text
 from summary import summary
 
 INPUT_AUDIO_FOLDER = Path("/workspaces/MurmurMiner/input_audios")
-OUTPUT_FOLDER = Path("/workspaces/MurmurMiner/output_files")
-ONLY_SPEECH_FOLDER = "only_speech"
-SUMMARY_TEXT_FOLDER = "summary_text"
+OUTPUT_SUMMARY_FOLDER = Path("/workspaces/MurmurMiner/output_summaries")
 AUDIO_EXTENSIONS = [".mp3", ".wav", ".m4a"]
+SAMPLING_RATE = 16000
 
 
 def main():
     for audio_file in INPUT_AUDIO_FOLDER.iterdir():
         if audio_file.is_file() and audio_file.suffix.lower() in AUDIO_EXTENSIONS:
             print("----------remove_silence----------")
-            output_path = os.path.join(OUTPUT_FOLDER, ONLY_SPEECH_FOLDER, f"{audio_file.stem}.mp3")
-            remove_silence(audio_file, output_path)
-            print(output_path)
+            no_silence_audio = remove_silence(audio_file)
 
             print("----------speech_to_text----------")
-            transcribed_text = speech_to_text(output_path)
+            transcribed_text = speech_to_text(no_silence_audio)
             print(transcribed_text)
 
             print("----------summary----------")
-            output_path = os.path.join(OUTPUT_FOLDER, SUMMARY_TEXT_FOLDER, f"{audio_file.stem}.txt")
-            summary_text = summary(transcribed_text, output_path)
+            summary_text = summary(transcribed_text)
             print(summary_text)
 
-            print("----------完了----------")
+            output_path = OUTPUT_SUMMARY_FOLDER / f"{audio_file.stem}.txt"
+            with open(output_path, "w", encoding="utf-8") as f:
+                f.write(summary_text)
+
+            print(f"----------{audio_file}: 完了----------")
 
 
 if __name__ == "__main__":
